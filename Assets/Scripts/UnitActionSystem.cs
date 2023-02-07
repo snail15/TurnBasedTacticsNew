be4a60ba5c10f70500ae5129bfcdc7d19aff14b9
snail15 <jungs@uchicago.edu>
2023-02-07 00:13:31 -0800

    Unit Selection Visual

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -4,9 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour {
+    
+    public static UnitActionSystem Instance { get; private set; }
+
+    public event EventHandler OnSelectedUnitChanged;
+    
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
     private void Update()
     {
         
@@ -24,11 +38,21 @@ public class UnitActionSystem : MonoBehaviour {
         {
             if (raycast.transform.TryGetComponent<Unit>(out Unit unit))
             {
-                selectedUnit = unit;
+                SetSelectedUnit(unit);
                 return true;
             }
         }
 
         return false;
+    }
+    private void SetSelectedUnit(Unit unit)
+    {
+        selectedUnit = unit;
+        OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public Unit GetSelectedUnit()
+    {
+        return selectedUnit;
     }
 }
