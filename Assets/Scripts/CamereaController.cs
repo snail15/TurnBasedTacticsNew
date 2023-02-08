86dd3e94b8f30b11c69b2d96485acf696079f447
snail15 <jungs@uchicago.edu>
2023-02-07 23:55:11 -0800

    Camera Zoom

--- Assets/Scripts/CamereaController.cs
+++ Assets/Scripts/CamereaController.cs
@@ -1,12 +1,70 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
-public class CamereaController : MonoBehaviour
-{
+public class CamereaController : MonoBehaviour {
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+    private Vector3 _targetFollowOffset;
+    private CinemachineTransposer _cinemachineTransposer;
+    private void Start()
+    {
+        _cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        _targetFollowOffset = _cinemachineTransposer.m_FollowOffset;
+    }
     private void Update()
     {
+        HandleCameraMovement();
+
+        HandleCameraRotation();
+
+        HandleZoom();
+    }
+    private void HandleZoom()
+    {
+
+        //var cineMachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+
+        //Vector3 followOffset = cineMachineTransposer.m_FollowOffset;
+
+        float zoomAmount = 1f;
+        if (Input.mouseScrollDelta.y > 0)
+        {
+            _targetFollowOffset.y -= zoomAmount;
+        }
+        if (Input.mouseScrollDelta.y < 0)
+        {
+            _targetFollowOffset.y += zoomAmount;
+        }
+
+        _targetFollowOffset.y = Mathf.Clamp(_targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+
+        float zoomSpeed = 5f;
+        _cinemachineTransposer.m_FollowOffset =
+            Vector3.Lerp(_cinemachineTransposer.m_FollowOffset, _targetFollowOffset, Time.deltaTime * zoomSpeed);
+    }
+    private void HandleCameraRotation()
+    {
+
+        Vector3 rotationVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y = +1f;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y = -1f;
+        }
+        float rotationSpeed = 100f;
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+    private void HandleCameraMovement()
+    {
+
         Vector3 inputMoveDir = new Vector3(0, 0, 0);
         if (Input.GetKey(KeyCode.W))
         {
@@ -24,22 +82,9 @@ public class CamereaController : MonoBehaviour
         {
             inputMoveDir.x = +1f;
         }
-        
+
         float moveSpeed = 10f;
         Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
-        
-        Vector3 rotationVector = Vector3.zero;
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotationVector.y = +1f;
-        }
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotationVector.y = -1f;
-        }
-        float rotationSpeed = 100f;
-        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
     }
 }
